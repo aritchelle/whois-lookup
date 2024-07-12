ef38c4851d25a82e68ddd70990509b7863c2f050
aritchelle <calaritchelle@gmail.com>
2024-07-12 13:15:53 +0800

    format date

--- app/components/DataTable.tsx
+++ app/components/DataTable.tsx
@@ -8,7 +8,49 @@ interface DataTableProps {
 const DataTable: React.FC<DataTableProps> = ({ headers, data }) => {
     const formatHostnames = (hostnames: string[]): string => {
         return hostnames.map(hostname => (hostname.length > 25 ? `${hostname.substring(0, 25)}...` : hostname)).join(', ');
-      };
+    };
+    
+    
+/**
+ * format the data, especially for date that will be more readable, UNDEFINED on INTL..
+ * @param {header} - The header of the table.
+ * @param {itemData} - the item to be formated.
+ * @returns {formatedItem} The formated data to be display on the table.
+ */
+    const dataFormat= (header: string, itemData: string ) => {
+
+        let formatedItem: string;
+
+        /**
+         * format the date, especially for date that will be more readable, UNDEFINED on INTL..
+         * {undefined} - Insure that the date will be displayed base on the user locale.
+         * ex. people with 
+         * 'en-GB' expected output 12 July 2024
+         * 'en-US' expected output July 12 2024
+         * 'ja-JP' will result to Japanese format as well
+         */
+        const formatDate = (dateString: string) => {
+            const date = new Date(dateString);
+            return new Intl.DateTimeFormat(undefined, { 
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            }).format(date);
+        };
+    
+        switch(header) {
+            case 'Hostnames':
+                formatedItem = formatHostnames(itemData.split(', '));
+                break;
+            case 'Registration Date':
+            case 'Expiration Date':
+                formatedItem = formatDate(itemData);
+                break;
+            default:
+                formatedItem = itemData;
+        }
+        return formatedItem;
+    }
   return (
     <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg mt-2">
       <table className="min-w-full divide-y divide-gray-200">
@@ -25,7 +67,7 @@ const DataTable: React.FC<DataTableProps> = ({ headers, data }) => {
           <tr>
             {data.map((item, index) => (
               <td key={index} className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {headers[index] === 'Hostnames' ? formatHostnames(item.split(', ')) : item}
+                {dataFormat(headers[index], item)}
               </td>
             ))}
           </tr>
